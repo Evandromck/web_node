1e03ad6cf540793525dc50cc8f13ef233861756a
Evandromck <evandroaraujomck@gmail.com>
2023-06-06 13:14:00 -0300

    Deploy front-end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { Button } from "@/componets/Button";
 
 export default function Home() {
   return (
-    <h1 className="uppercase text-2xl font-bold">teg
-      
+    <h1 className="h-screen bg-zinc-900 text-zinc-50 pa">
+      teg      
     </h1>
      )
 }
